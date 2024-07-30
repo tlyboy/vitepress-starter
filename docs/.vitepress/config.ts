ea66930986b50cc9f3eeceea775769a408f0ab81
Guany <1084072690@qq.com>
2024-07-30 10:59:54 +0800

    feat: initial

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,81 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'VitePress starter',
+  description: '🚀 VitePress starter',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+      },
+    ],
+  ],
+  lang: 'zh-CN',
+  lastUpdated: true,
+  markdown: {
+    theme: {
+      light: 'vitesse-light',
+      dark: 'vitesse-dark',
+    },
+    lineNumbers: true,
+  },
+  themeConfig: {
+    logo: '/img/logo.svg',
+    siteTitle: 'VitePress starter',
+    nav: [
+      {
+        text: '指南',
+        link: '/guide/getting-started',
+      },
+    ],
+    sidebar: {
+      '/guide/': {
+        base: '/guide/',
+        items: [
+          {
+            text: '指南',
+            collapsed: false,
+            items: [
+              {
+                text: '简介',
+                link: 'getting-started',
+              },
+            ],
+          },
+        ],
+      },
+    },
+    outline: {
+      label: '页面导航',
+    },
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/tlyboy/vitepress-starter',
+      },
+    ],
+    footer: {
+      message: '基于 MIT 许可发布',
+      copyright: '版权所有 © 2023-present Guany',
+    },
+    editLink: {
+      pattern:
+        'https://github.com/tlyboy/vitepress-starter/edit/main/src/:path',
+      text: '在 GitHub 上编辑此页面',
+    },
+    lastUpdated: {
+      text: '最后更新于',
+    },
+    docFooter: {
+      prev: '上一页',
+      next: '下一页',
+    },
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+    sidebarMenuLabel: '菜单',
+    returnToTopLabel: '回到顶部',
+  },
+})
---
+++ docs/guide/getting-started.md
@@ -0,0 +1,13 @@
+# vitepress-starter
+
+🚀 VitePress starter
+
+## 使用说明
+
+```sh
+npx degit tlyboy/vitepress-starter my-new-project
+```
+
+## 使用许可
+
+[MIT](https://opensource.org/licenses/MIT) © Guany
